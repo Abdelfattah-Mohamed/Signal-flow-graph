69e62c018f46578aa126d76b2599350113449cca
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2019-03-16 23:06:36 +0200

    graph

---
+++ src/Graph.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+
+public class Graph {
+
+	private boolean[][] graph;
+	private int[][] gain;
+
+	public Graph(int nV) {
+		this.graph = new boolean[nV][nV];
+		this.gain = new int[nV][nV];
+	}
+
+	public boolean[][] getGraph() {
+		return this.graph;
+	}
+
+	public void addEdge(int f, int t, int gain) {
+		if (gain != 0) {
+			this.graph[f][t] = true;
+			this.gain[f][t] = gain;
+		}
+	}
+
+	public void printPaths() {
+		Paths path = new Paths();
+		LinkedList<LinkedList<Integer>> list = path.getPaths(this.graph, this.graph.length);
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.get(i).size(); j++) {
+				System.out.print(list.get(i).get(j) + "=>");
+			}
+			System.out.println();
+		}
+	}
+}
