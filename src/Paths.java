3e2f439950b4544f48d2129cafdd449176297b82
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2019-03-16 23:07:06 +0200

    paths

---
+++ src/Paths.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class Paths {
+
+	private LinkedList<LinkedList<Integer>> paths = new LinkedList<LinkedList<Integer>>();
+
+	private void dfs(int from, int distance, boolean[][] graph, LinkedList<Integer> path) {
+		if (from == distance) {
+			paths.add((LinkedList<Integer>) path.clone());
+			path.removeLast();
+			return;
+		}
+		for (int i = from + 1; i < graph.length; i++) {
+			if (graph[from][i]) {
+				path.add(i);
+				this.dfs(i, distance, graph, path);
+			}
+
+		}
+		path.removeLast();
+		return;
+	}
+
+	public LinkedList<LinkedList<Integer>> getPaths(boolean[][] graph, int distance) {
+		LinkedList<Integer> path = new LinkedList<Integer>();
+		if (distance > 0) {
+			path.add(0);
+			this.dfs(0, distance-1, graph, path);
+		}
+		return this.paths;
+	}
+
+}
